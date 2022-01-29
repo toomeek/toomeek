eb2bca6a0a02be8a2af0a74ff6c8ea6181ebf572
tooommeeek <tooommeeek@wp.pl>
2022-01-29 17:15:26 +0100

    Navigation bar created

---
+++ life-manager/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavBarComponent} from './nav-bar/nav-bar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
+@NgModule({
+  declarations: [AppComponent,
+    NavBarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonToggleModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+  ;
+}
